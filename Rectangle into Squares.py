c707551aad06913bcd1172f181c73c09e635eefa
yann <tmp4321@qq.com>
2017-07-05 19:45:39 +0800

    Rectangle into Squares 6th lambda

--- Rectangle into Squares.py
+++ Rectangle into Squares.py
@@ -1,12 +1,7 @@
-def sqInRect(lng, wdth):
-    if lng !=wdth:
-        sqrs = []
-        while lng and wdth:
-            if wdth > lng:
-                lng,wdth = wdth ,lng
-            sqrs.append(wdth)
-            lng -=wdth
-        return sqrs
+#sqInRect=s=lambda a,b,r=0:(None,[a][r]if a==b else [min(a,b)]+s(min(a,b),abs(a-b),1)
+sqInRect=s=lambda a,b,r=0 : (None,[a])[r] if a==b else [min(a,b)]+s(min(a,b),abs(a-b),1)
 
-# 写法类似 while循环,小值互换
-# 自减的用法和不返回表示None值的做法值得学习
+# 这也叫人写的东西?
+# 原理上和递归那一个的写法类似,但是lambda不是很熟,只能领会,解释不完全
+# 也是不相等的时候,返回ab的较小值,和相减的绝对值,并递归,只到相等
+# if a==b取前式, 否则取后式计算
