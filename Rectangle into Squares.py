e0d2879ed729da6d8506cb308f710e77d28fb58c
yann <tmp4321@qq.com>
2017-07-05 17:45:03 +0800

    Rectangle into Squares 3rd sorted switch

--- Rectangle into Squares.py
+++ Rectangle into Squares.py
@@ -1,19 +1,16 @@
-def sqInRect(lng, wdth):
-    if lng == wdth:
-        return None
-    if lng < wdth:
-        wdth, lng = lng, wdth
-    res = []
-    while lng != wdth:
-        res.append(wdth)
-        lng = lng - wdth
-        if lng < wdth:
-            wdth, lng = lng, wdth
-    res.append(wdth)
-    return res
+def sqInRect(a, b):
+	if a == b:
+		return None
+
+	res = []
+
+	while b:
+		b, a = sorted([a, b])
+		res +=[b]
+		a, b = b, a-b
+
+	return res
 
 
-# 题没有理解,确实是不停的长宽想减,但是在不相等的条件下考虑
-# 结尾处考虑不到, 过度考虑面积与大数优先,没注意图例
-# 2个参数大小变化后互换,想到了, while函数想到了,但是还不足
-# 思考方向错了,基于遍历乘积考虑的,没注意更简便的方法
+# 原理同上, 一样是返回b, 和a,b之差
+# 技巧之处是 用了 b, a = sorted([a,b])保证b是较小值
