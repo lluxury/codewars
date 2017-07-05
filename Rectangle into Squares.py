e9218272b18054a1eb9c4e012aba4522d00034d9
yann <tmp4321@qq.com>
2017-07-05 17:13:38 +0800

    Rectangle into Squares 2nd a better solution

--- Rectangle into Squares.py
+++ Rectangle into Squares.py
@@ -1,61 +1,19 @@
 def sqInRect(lng, wdth):
     if lng == wdth:
-    # your code
         return None
-    
-    elif lng > wdth:
-        pass
-        result = []
-        other = lng * wdth - wdth * wdth
-        #print(other)
-        result.append(wdth)
-        while other - wdth * wdth >=2:
-            other = other - wdth * wdth
-            result.append(wdth)
-        while other - wdth * wdth < 2:
-            for wdth_1 in range(wdth,0,-1):
-                if other - wdth_1 * wdth_1 > 2:
-                    other = other - wdth_1 * wdth_1
-                    result.append(wdth_1)
-                else:
-                    continue
-            
-        
-        
-        
-        for wdth_1 in range(wdth,0,-1):
-            
-                if other - wdth_1 * wdth_1 >2:
-                    result.append(wdth_1)
-                    other = other - wdth_1 * wdth_1
-                    
-                    
-            x=(other - wdth_1*wdth_1)
-            #print(x)
-            if str(x).isdigit():
-                result.append(wdth_1)
-                print(wdth_1)
-            #print(wdth_1)
-            wdth_1 = wdth - 1
-            if wdth_1 >=2 and (other - (wdth_1 * wdth_1) >=2):
-                result.append(wdth_1)
-                result.append(1)
-                result.append(1)
-                pass
-        return result    
-        
-    else:
-        #pass
-        result = []
-        other = lng * wdth - lng * lng
-        #print(other)
-        result.append(lng)
-        for wdth_1 in range(lng,2,-1):
-            print(wdth_1)
-            lng_1 = lng - 1
-            if lng_1 >=2 and (other - (lng_1 * lng_1) >=2):
-                result.append(lng_1)
-                result.append(1)
-                result.append(1)
-                pass
-        return result
+    if lng < wdth:
+        wdth, lng = lng, wdth
+    res = []
+    while lng != wdth:
+        res.append(wdth)
+        lng = lng - wdth
+        if lng < wdth:
+            wdth, lng = lng, wdth
+    res.append(wdth)
+    return res
+
+
+# 题没有理解,确实是不停的长宽想减,但是在不相等的条件下考虑
+# 结尾处考虑不到, 过度考虑面积与大数优先,没注意图例
+# 2个参数大小变化后互换,想到了, while函数想到了,但是还不足
+# 思考方向错了,基于遍历乘积考虑的,没注意更简便的方法
