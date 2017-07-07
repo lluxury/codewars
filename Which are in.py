58abdac6c47d0ef94ebe8767209d0856706306d8
yann <tmp4321@qq.com>
2017-07-07 17:01:11 +0800

    Which are in 2nd best normal

--- Which are in.py
+++ Which are in.py
@@ -1,21 +1,11 @@
 def in_array(array1, array2):
-    r = []
-    t = []
-    for x in array1:
-        for y in array2:
-            if x in y:
-                #z=set(x)
-                #print (x)
-                r.append(x)
-                for z in r:
-                #r.sort()
-                #for z in r:
-                    if z not in t:
-                        t.append(z)
-                        t.sort()
-        continue
-    # your code
-    return t
+    res = []
+    for a1 in array1:
+        for a2 in array2:
+            if a1 in a2 and not a1 in res:
+                res.append(a1)
+    res.sort()
+    return res
 
-# 巨蠢流解法,好坏是解出来了
-# 偷了别人一个用法,完成了list去重
+# 总算接近正常答案了,不容易
+# 学到了同时对目标和结果判断的去重方式
