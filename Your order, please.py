293d2e9b8115056b742a61aea70dc61ae92f0875
yann <tmp4321@qq.com>
2017-07-13 18:49:37 +0800

    Your order, please 2nd normal solution

--- Your order, please.py
+++ Your order, please.py
@@ -1,23 +1,22 @@
 def order(sentence):
-    if sentence == "":
-        return sentence
-    n = 0
-    sentence_list = sentence.split(" ")
-    res=[]
-    for x in range(len(sentence_list)):
-        res.append(" ")
-    while n < len(sentence_list):
-        #n + =1
-        m = n +1
-        for word in sentence_list:
-            if str(m) in word:
-                res[n] = word
-            else:
-                continue
-        n+=1
-        #print(res)
-    return " ".join(res)
+    '''
+    >>> order("is2 Thi1s T4est 3a")
+    'Thi1s is2 3a T4est'
+    '''
+    if not sentence:
+        return ""
+    result = []    #the list that will eventually become our setence
+      
+    split_up = sentence.split() #the original sentence turned into a list
+  
+    for i in range(1,10):
+        for item in split_up:
+            if str(i) in item:
+                 result.append(item)    #adds them in numerical order since it cycles through i first
+  
+    return " ".join(result)
 
-# 虽然写的比较啰嗦,还是解出来了
-# 挖出一个大盲点: 列表必须要按顺序操作,append和提前建好空列表再改值是比较靠谱的方法
-# 如果随机插入,不连续,需要用字典
+# 比较正常的解法 判空返回
+# 分割字符串, 直接写了范围1,10, 貌似题目有给,我没仔细看
+# 按序匹配,插入列表,但是并没有空值的处理,我理解错了?
+# 论读清楚题的重要性,难度下除指数级
