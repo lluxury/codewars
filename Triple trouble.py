c0d5f3c170232c7fb9b9ffe3ea5a6f0328d79dff
yann <tmp4321@qq.com>
2017-07-14 17:23:02 +0800

    Your order, please 3rd lambda x

--- Triple trouble.py
+++ Triple trouble.py
@@ -8,3 +8,45 @@ def triple_double(num1, num2):
 
 # 模仿昨天的解法写出的1行等式,一个范围,2阶判断的模型,反回的是逻辑值
 # 错误,'123'是字符串, num1,num2才是int, 概念不清,混淆了需要转换的对象
+
+
+
++def sqInRect(lng, wdth, recur = 0):
+ -    if a == b:         +    if lng == wdth:
+ -        return None         +        #return (None, [lng][recur])
+ +        return (None, [lng])[recur]        #return(None,[1])[0]
+            
+ -    res = []         +    #lessen = min(lng, wdth)
+ +    lesser = min(lng, wdth)
+ +    return [lesser] + sqInRect(lesser, abs(lng - wdth), recur = 1)
+
+
+
+def digital_root(n, j=0):
+   #print(str(n))
+   while n >10:
+       for i in '0123456789':
+           if i in str(n):
+               j+=int(i)
+               #print(i)
+               n=j               
+   return j
+   
+    
+def     digital_root(n, j=0,recur =0):
+    if j >10 :
+
+
+def digital_root(n, j=0,s=1):
+    print(str(n))
+    while s:
+        #for i in '0123456789':
+        for i in str(n):
+            #if i in str(n):
+            j+=int(i)
+            print(j)
+        if j<10:
+            s=0
+        n=j
+    return n
+print(digital_root(93))
--- Your order, please.py
+++ Your order, please.py
@@ -3,20 +3,10 @@ def order(sentence):
     >>> order("is2 Thi1s T4est 3a")
     'Thi1s is2 3a T4est'
     '''
-    if not sentence:
-        return ""
-    result = []    #the list that will eventually become our setence
-      
-    split_up = sentence.split() #the original sentence turned into a list
-  
-    for i in range(1,10):
-        for item in split_up:
-            if str(i) in item:
-                 result.append(item)    #adds them in numerical order since it cycles through i first
-  
-    return " ".join(result)
+    return " ".join(sorted(sentence.split(), key=lambda x: int(filter(str.isdigit, x))))
 
-# 比较正常的解法 判空返回
-# 分割字符串, 直接写了范围1,10, 貌似题目有给,我没仔细看
-# 按序匹配,插入列表,但是并没有空值的处理,我理解错了?
-# 论读清楚题的重要性,难度下除指数级
+# 又是传说中的一行过,我啥时候能写出这么叼的代码呢
+# 反回,拼接,排序,列表,按关键字排 x ,x变量值,ps2的用法,这种的还是要多写
+# 学到了2个方法 用key排序,用isdigit判断数字
+# : 后面全部是表达式,为了生成一个合适的排序关键字
+# filter()也接收一个函数和一个序列 把传入的函数依次作用于每个元素 根据返回值是True还是False决定保留还是丢弃该元素
