2acde6a99ea50cc0232b098e5e0a5d1b7125b01a
yann <tmp4321@qq.com>
2017-07-17 10:28:00 +0800

    Simple Pig Latin 2nd simple way

--- Simple Pig Latin.py
+++ Simple Pig Latin.py
@@ -1,30 +1,5 @@
 def pig_it(text):
-    #your code here
-    res=[]
-    pig_it_list=text.split()
-    for word in pig_it_list:
-        #if word.alpha():
-        if word.encode( 'UTF-8' ).isalpha():
-            word_new=word[1:]
-            word_new=word_new+word[:1]+'ay'
-            #print(type(word))
-            #word_l=list(word)
-            #list(word)
-            #print(type(list(word)))
-            #word_d=word[:]
-            #print(type(word_d))
-            #word.pop(list(word_d)[:1])
-            #word.append(list(word_d)[:1])
-            #word.append("ay")
-            #word_r="".join(word)
-            res.append(word_new)
-        else:        
-            res.append(word)
-            break
-    return ' '.join(res)
+    return ' '.join([x[1:]+x[0]+'ay' if x.isalpha() else x for x in text.split()])
 
-
-# 虽然写的很2,还是过了
-# 注意区别list方法和str方法及级变量的保存方法
-# list不能直接操作以后,赋值给别一个list #word_d=word[:]
-# py3里面是否是字符的方法写法有变
+# 其实就是2个字符串过滤拼接,比移动方便多了,思路巧妙
+# a if xx else b, 单行判断处理异常字符,xx为判断,标准套路
