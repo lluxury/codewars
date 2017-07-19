51fbceb01d458e646a72f4e156ef051ee594525a
yann <tmp4321@qq.com>
2017-07-19 15:32:07 +0800

    DNA_strand 2nd maketrans

--- DNA_strand.py
+++ DNA_strand.py
@@ -1,40 +1,8 @@
-
-import unittest
-
+import string
 def DNA_strand(dna):
-    # code here
-    str = [i for i in dna]
-    #print (str)
-    n = len(str)
-    dst = []
-    #print (n)
-    #while n >0 :
-    for s in str:
-        if s =='A':
-            s = 'T'
-            dst.append(s)
-            print(s)
-            continue
-        elif s == 'T':
-            s = 'A'
-            dst.append(s)
-            continue
-        elif s == 'G':
-            s = 'C'
-            dst.append(s)
-            continue
-        elif s == 'C':
-            s = 'G'
-            dst.append(s)
-            continue
-    return ''.join(dst)
+    return dna.translate(string.maketrans("ATCG","TAGC"))
 
-
-class SumTestCase(unittest.TestCase):
-
-    def test_summary(self):
-        '''能够正确处理获得结果'''
-        #self.assert_equal(digital_root(16),7)
-        self.assertEqual(DNA_strand("AAAA"),"TTTT","String AAAA is")
-
-unittest.main()
+# 翻译功能和字符串的转换功能,但我不知道这2个方法的区别
+# 过滤 和翻译
+# str.maketrans(x[, y[, z]])
+# str.translate(table)
