d28e4ca5b7b8f596f23d690fe39870c4474d063a
yann <tmp4321@qq.com>
2017-07-19 16:56:05 +0800

    Growth of a Population 2nd normal

--- Growth of a Population.py
+++ Growth of a Population.py
@@ -1,8 +1,9 @@
-def nb_year(p0, percent, aug, p):
-    # your code
-    n = 0
-    while p0 < p:
-        p0 = p0 * (1 + percent/100) + aug
-        print (p0)
-        n+= 1
-    return n
+def nb_year(population, percent, aug, target):
+    year = 0
+    while population < target:
+        population += population * percent / 100. + aug
+        year +=1
+    return year
+nb_year(1500, 5, 100, 5000)
+
+# 比较标准的写法,就是不知道100.是啥意思
